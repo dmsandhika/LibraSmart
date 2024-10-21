2c5a04e6d5bdfbaf452bee4bbae6edbd90839170
Dimas Andhika Firmansyah <evandimas818@gmail.com>
2024-10-21 21:12:29 +0700

    add migration

---
+++ database/migrations/2024_10_21_135532_create_book_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('book', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->string('author');
+            $table->string('isbn')->unique();
+            $table->string('cover');
+            $table->unsignedBigInteger('category_id');
+            $table->string('description')->nullable();
+            $table->integer('stock')->default(0);
+            $table->timestamps();
+            
+            $table->foreign('category_id')->references('id')->on('category');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('book');
+    }
+};
